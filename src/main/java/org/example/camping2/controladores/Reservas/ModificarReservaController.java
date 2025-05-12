2d49f4639a54ac88ab9f0b2c0a39fc97947e84f0
andrews <andrews@gestionlan.com>
2025-05-12 11:52:16 +0200

    mostrar Recursos en tabla modificar ya funcionando

--- src/main/java/org/example/camping2/controladores/Reservas/ModificarReservaController.java
+++ src/main/java/org/example/camping2/controladores/Reservas/ModificarReservaController.java
@@ -1,10 +1,67 @@
 package org.example.camping2.controladores.Reservas;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import org.example.camping2.modelo.dto.Reserva;
 import org.example.camping2.modelo.memoria.Memoria;
 
 public class ModificarReservaController {
     Memoria<Reserva, Integer> memoriaReserva;
+
+    @FXML
+    private DatePicker fechaInicio;
+    @FXML
+    private DatePicker fechaFin;
+    @FXML
+    private TextField precioText;
+    @FXML
+    private ComboBox estadoCombo;
+
+    ObservableList<String> tipos = FXCollections.observableArrayList("ACTIVA", "FINALIZADA", "CANCELADA");
+    @FXML
+    private TableView<Reserva> reservaTable;
+    @FXML
+    private TableColumn<Reserva, Integer> idColumn;
+    @FXML
+    private TableColumn<Reserva, String> fechaInicioColumn;
+    @FXML
+    private TableColumn<Reserva, String> fechaFinColumn;
+    @FXML
+    private TableColumn<Reserva, String> precioColumn;
+    @FXML
+    private TableColumn<Reserva, String> nombreColumn;
+    @FXML
+    private TableColumn<Reserva, String> apellidoColumn;
+    @FXML
+    private TableColumn<Reserva, String> dniColumn;
+
+    private ObservableList<Reserva> Reserva = FXCollections.observableArrayList();
+
+
+    @FXML
+    public void initialize() {
+        estadoCombo.setItems(tipos);
+        estadoCombo.getSelectionModel().selectFirst();
+        reservaTable.setItems(Reserva);
+        idColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getId()));
+        fechaInicioColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getFechaInicio().toString()));
+        fechaFinColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getFechaFin().toString()));
+        precioColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getPrecioTotal().toString()));
+        nombreColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getIdcliente().getNombre()));
+        apellidoColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getIdcliente().getApellido()));
+        dniColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getIdcliente().getDni()));
+    }
+
+    @FXML
+    public void cargarTodos(){
+        Reserva.clear();
+        Reserva.addAll(memoriaReserva.findAll());
+    }
+
+
 
     public Memoria<Reserva, Integer> getMemoriaReserva() {
         return memoriaReserva;
